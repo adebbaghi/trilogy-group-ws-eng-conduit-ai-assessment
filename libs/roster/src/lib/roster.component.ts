a74269cb5cdcde5f2577348650c80bea14104956
adebbaghi <abde.de71@gmail.com>
2025-05-24 18:19:25 +0200

    Update roster.component.ts

--- libs/roster/src/lib/roster.component.ts
+++ libs/roster/src/lib/roster.component.ts
@@ -13,14 +13,14 @@ interface RosterEntry {
 @Component({
   selector: 'realworld-roster',
   templateUrl: './roster.component.html',
-  styleUrls: [],
+  styleUrls: []
 })
 export class RosterComponent implements OnInit {
   roster$!: Observable<RosterEntry[]>;
 
   constructor(private rosterService: RosterService) {}
 
-  ngOnInit() {
+  ngOnInit(): void {
     this.roster$ = this.rosterService.getRoster();
   }
 }
